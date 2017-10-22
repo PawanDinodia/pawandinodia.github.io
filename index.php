f2d1a86af73ef7d4d69131116c4ff66f464c941c
Pawan Dinodia <pawandinodiabme@gmail.com>
2017-10-22 16:23:03 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" content="text/html">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script type="text/javascript" src="include/js/jquery.js"></script>
+  <script type="text/javascript" src="include/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="include/css/bootstrap.min.css"></style>
+  <link rel="stylesheet" href="include/css/animate.min.css"></style>
+  <link href="https://fonts.googleapis.com/css?family=Creepster" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+  <link type="image/x-icon" href="img/img6.png" rel="icon">
+  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+  <div class="container-fluid main">
+      <div class="navigation-bar">
+      </div>
+
+<div class="intro">
+  <center><div class="profile-pic animated infinite">
+      <a href="javascript:void(0);">
+        <img class="img-rounded img-responsive img-circle profile-img" src="img/pic.png" alt="profile pic" title="Click to join me on instagram">
+      </a>
+  </div></center>
+<div class="name"><center>Pawan Dinodia</center></div>
+  <div class="social">
+
+    <center><ul>
+      <li><a href="javascript:void(0);"><i class="fa fa-facebook"></i></a>
+      </li>
+      <li><a href="javascript:void(0);"><i class="fa fa fa-twitter"></i></a>
+      </li>
+      <li><a href="javascript:void(0);"><i class="fa fa-linkedin"></i></a>
+      </li>
+      <li><a href="javascript:void(0);"><i class="fa fa-google-plus"></i></a>
+      </li>
+      <li><a href="javascript:void(0);"><i class="fa fa-github"></i></a>
+      </li>
+      <li><a href="javascript:void(0);"><i class="fa fa-free-code-camp"></i></a>
+      </li>
+    </ul></center>
+
+  </div>
+</div>
+
+<div class="heading">
+  <hr><center>About</center><hr>
+</div>
+
+<div class="intro-text"
+<div class="profession-tiles">
+<div class="col-md-1"></div>
+<a href="javascript:void(0)">
+<div class="profession col-md-3"><center>
+  <div class="profession-heading">Bio-Medical Engineer</div>
+  <div class="profession-icon"><i class="fa fa-heartbeat animated infinite pulse"></i></div>
+  <div class="profession-description container-fluid">I am persuing my Graduation in Bio-Medical engineering from <a href="javascript:void(0);">DCRUST University</a>.</div>
+</center></div></a>
+
+<a href="javascript:void(0)">
+<div class="profession col-md-3"><center>
+  <div class="profession-heading">WEB Developer</div>
+  <div class="profession-icon"><i class="fa fa-microchip animated infinite flip"></i></div>
+  <div class="profession-description container-fluid">I am enthusiastic and self-learned Web-developer Learned from various sources.</div>
+</center></div></a>
+
+<a href="javascript:void(0)">
+<div class="profession col-md-3"><center>
+  <div class="profession-heading">Android-developer Developer</div>
+  <div class="profession-icon"><i class="fa fa-android animated infinite bounce"></i></div>
+  <div class="profession-description container-fluid">I am skilled Andorid Develper also self learned from various sources.</div>
+</center></div></a>
+<div class="col-md-1"></div>
+</div>
+</div>
+
+<div class="heading">
+  <hr><center>Projects</center><hr>
+</div>
+
+<div class="sub-heading">
+  <center>Mini-Projects</center>
+</div>
+
+  <div class=" col-xs-4">
+<div class="project-tiles">
+  <center>
+  <div class="project-title">Trabute Page
+  </div>
+  <div class="project-image">
+    <img alt="Tabute to Om-puri" title="Trabute to Om-puri!" src="img/trabute-page.png">
+  </div>
+  <div class="project-description">
+    This project is Trabute to a great indian actor Om-Puri!
+  </div>
+</center>
+</div>
+</div>
+
+<div class="col-xs-4">
+<div class="project-tiles">
+  <center>
+  <div class="project-title">Random Quote Machine
+  </div>
+  <div class="project-image">
+    <img alt="Random Quote Machine" title="Random Quote Machine!" src="img/quote-machine.png">
+  </div>
+  <div class="project-description">
+    A random quote genrator machine. You can also tweet Quotes directly from here!.
+  </div>
+</center>
+</div>
+</div>
+
+<div class=" col-xs-4">
+<div class="project-tiles">
+  <center>
+  <div class="project-title">Wikipedia Viewer
+  </div>
+  <div class="project-image">
+    <img alt="A Wikipedia Viewer" title="A wikipedia viewer!" src="img/wikipedia_viewer.png">
+  </div>
+  <div class="project-description">
+    This is simple wikipedia viewer using Wikipedia api. You can search for keywords and it will show the relevant results.
+  </div>
+</center>
+</div>
+</div>
+
+<div class=" col-xs-4">
+<div class="project-tiles">
+  <center>
+  <div class="project-title">Weather app
+  </div>
+  <div class="project-image">
+    <img alt="Weather App" title="Weather app!" src="img/weather_app.png">
+  </div>
+  <div class="project-description">
+    This Application shows local Weather using Open weather app api.
+  </div>
+</center>
+</div>
+</div>
+
+  </div>
+</body>
+</html>
